79323301483a84d21b053d67c3b7312888342323
Charles Ferreira <charles.dev@live.com>
2021-11-19 16:44:14 -0300

    Refactor the ObjectsController
    Move the dependency injection of the API service from the index()
    method to the constructor, to reuse it on the next methods

--- office-art-app/app/Http/Controllers/ObjectsController.php
+++ office-art-app/app/Http/Controllers/ObjectsController.php
@@ -7,13 +7,25 @@ use App\Service\MetMuseumAPIService;
 class ObjectsController extends Controller
 {
     /**
+     * @var MetMuseumAPIService
+     */
+    private $service;
+
+    /**
+     * @param MetMuseumAPIService $service
+     */
+    public function __construct(MetMuseumAPIService $service) {
+        $this->service = $service;
+    }
+
+    /**
      * Fetches a listing of all valid Object IDs available for access at the
      * Metropolitan Museum of Art Collection API
      *
      * @param MetMuseumAPIService $service
      * @return array
      */
-    public function index(MetMuseumAPIService $service) {
-        return $service->objects();
+    public function index() {
+        return $this->service->objects();
     }
 }
