c60c8880910291814a365fac9e0c8fe5ffc5b5ef
Charles Ferreira <charles.dev@live.com>
2021-11-19 16:42:34 -0300

    Add test for loading an object's details

--- office-art-app/tests/Feature/ObjectsControllerTest.php
+++ office-art-app/tests/Feature/ObjectsControllerTest.php
@@ -8,17 +8,44 @@ use Tests\TestCase;
 
 class ObjectsControllerTest extends TestCase
 {
+    /**
+     * Tests that can get a list of objects from the API
+     */
     public function test_loading_the_object_list() {
-        $this->mock(MetMuseumAPIService::class, function (MockInterface $mock) {
-            $mock->shouldReceive('objects')->once()->andReturn([
-                'total' => 3,
-                'objectIDs' => [1, 2, 3]
-            ]);
+        $mockData = [
+            'total' => 3,
+            'objectIDs' => [1, 2, 3]
+        ];
+
+        $this->mock(MetMuseumAPIService::class, function (MockInterface $mock) use ($mockData) {
+            $mock->shouldReceive('objects')->once()->andReturn($mockData);
         });
 
         $response = $this->get('/api/objects');
 
         $response->assertStatus(200);
-        $response->assertJsonStructure(['total', 'objectIDs']);
+        $response->assertJsonStructure(array_keys($mockData));
+    }
+
+    /**
+     * Tests that can get an object's details from the API
+     */
+    public function test_loading_a_single_object() {
+        $mockData = [
+            'id' => 1,
+            'title' => 'A title',
+            'data' => 'A date',
+            'department' => 'A department',
+            'artist' => 'An artist'
+        ];
+
+        $this->mock(MetMuseumAPIService::class, function (MockInterface $mock) use ($mockData) {
+            $mock->shouldReceive('object')->once()->andReturn($mockData);
+        });
+
+        $response = $this->get('/api/objects/1');
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(array_keys($mockData));
     }
 }
