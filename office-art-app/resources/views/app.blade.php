0f6390223a3045a793156d1603f90be18653f10b
Charles Ferreira <charles.dev@live.com>
2021-11-19 02:46:46 -0300

    Create layout for sidebar

--- office-art-app/resources/views/app.blade.php
+++ office-art-app/resources/views/app.blade.php
@@ -10,15 +10,27 @@
 </head>
 
 <body>
-<div class="c-frame">
+<section class="c-frame">
     <!-- Image 1 -->
     <img id="bg-1" class="c-frame__image c-frame__image--background" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
     <img id="fg-1" class="c-frame__image c-frame__image--foreground" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
 
     <!-- Image 2 -->
-    <img id="bg-2" class="c-frame__image c-frame__image--background is-hidden" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
-    <img id="fg-2" class="c-frame__image c-frame__image--foreground is-hidden" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
-</div>
+    <img id="bg-2" class="c-frame__image c-frame__image--background is-hidden" src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/53459/208681/main-image" alt="">
+    <img id="fg-2" class="c-frame__image c-frame__image--foreground is-hidden" src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/53459/208681/main-image" alt="">
+</section>
+
+<aside class="c-info is-hidden">
+    <h1 class="c-info__title">Wheat Field with Cypresses</h1>
+    <p class="c-info__subtitle">
+        <time>1889</time>
+        | European Art
+    </p>
+    <p class="c-info__author">
+        <span class="c-info__author--name">Vincent van Gogh</span>
+        <span class="c-info__author--nationality">Dutch</span>
+    </p>
+</aside>
 </body>
 
 </html>
