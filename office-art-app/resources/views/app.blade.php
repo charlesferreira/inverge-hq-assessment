86b8f53a6cda22bdb2830464bdfb4493360bd1e8
Charles Ferreira <charles.dev@live.com>
2021-11-19 02:02:49 -0300

    Create layout for image frame

--- office-art-app/resources/views/app.blade.php
+++ office-art-app/resources/views/app.blade.php
@@ -1,12 +1,24 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Office Art</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
+
 <body>
-<h1>Welcome to Office Art</h1>
+<div class="c-frame">
+    <!-- Image 1 -->
+    <img id="bg-1" class="c-frame__image c-frame__image--background" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
+    <img id="fg-1" class="c-frame__image c-frame__image--foreground" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
+
+    <!-- Image 2 -->
+    <img id="bg-2" class="c-frame__image c-frame__image--background is-hidden" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
+    <img id="fg-2" class="c-frame__image c-frame__image--foreground is-hidden" src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="">
+</div>
 </body>
+
 </html>
