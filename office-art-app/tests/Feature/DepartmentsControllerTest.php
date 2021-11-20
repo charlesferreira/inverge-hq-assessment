c7977dec8a49121b36cf57dfea08aae0d61fde79
Charles Ferreira <charles.dev@live.com>
2021-11-19 23:30:21 -0300

    Add test for loading department list

---
+++ office-art-app/tests/Feature/DepartmentsControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\DTO\DepartmentList;
+use App\Service\MetMuseumAPIService;
+use Mockery\MockInterface;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
+use Tests\TestCase;
+
+class DepartmentsControllerTest extends TestCase
+{
+    /**
+     * Tests that can get a list of departments from the API
+     * @throws UnknownProperties
+     */
+    public function test_loading_the_departments_list() {
+        $mockData = new DepartmentList([
+            ['departmentId' => 1, 'displayName' => 'Department 1'],
+            ['departmentId' => 2, 'displayName' => 'Department 2']
+        ]);
+
+        $this->mock(MetMuseumAPIService::class, function (MockInterface $mock) use ($mockData) {
+            $mock->shouldReceive('departments')->once()->andReturn($mockData);
+        });
+
+        $response = $this->get('/api/departments');
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(array_keys($mockData->toArray()));
+    }
+}
